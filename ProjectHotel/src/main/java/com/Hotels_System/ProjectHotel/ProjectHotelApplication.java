58f96edbb2e00b903fc5205bb9223b6236d61c3f
Lucca Tourinho <luccatourinho@gmail.com>
2023-09-02 19:57:47 -0300

    Initial commit

---
+++ ProjectHotel/src/main/java/com/Hotels_System/ProjectHotel/ProjectHotelApplication.java
@@ -0,0 +1,13 @@
+package com.Hotels_System.ProjectHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHotelApplication.class, args);
+	}
+
+}
