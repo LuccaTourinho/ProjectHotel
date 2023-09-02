4f4b69ee141dfd13c9c241207145b01e3e6ff866
Lucca Tourinho <luccatourinho@gmail.com>
2023-09-02 20:52:50 -0300

    Firt day:
    Making the hotel repository

---
+++ ProjectHotel/src/main/java/com/Hotels_System/ProjectHotel/repository/HotelRepository.java
@@ -0,0 +1,7 @@
+package com.Hotels_System.ProjectHotel.repository;
+
+import com.Hotels_System.ProjectHotel.domain.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+}
