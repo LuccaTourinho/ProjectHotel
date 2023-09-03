2ac54b8fd23b783c37b2f609040868140df62aa1
Lucca Tourinho <luccatourinho@gmail.com>
2023-09-03 00:38:40 -0300

    Firt day:
    Updaters

--- ProjectHotel/src/main/java/com/Hotels_System/ProjectHotel/domain/Quality.java
+++ ProjectHotel/src/main/java/com/Hotels_System/ProjectHotel/domain/Quality.java
@@ -3,19 +3,9 @@ package com.Hotels_System.ProjectHotel.domain;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
-public enum Quality{
-    ECONOMIC(100),
-    STANDARD(250),
-    PREMIUM(500),
-    LUXURY(1000);
-
-    private final int basePrice;
-
-    Quality(int basePrice) {
-        this.basePrice = basePrice;
-    }
-
-    public int getBasePrice() {
-        return basePrice;
-    }
+public enum Quality {
+    ECONOMIC,
+    STANDARD,
+    PREMIUM,
+    LUXURY
 }
